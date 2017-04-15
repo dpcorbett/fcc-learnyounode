693b810cac38c83dfc30e9db09e164eeed72e120
Dave Corbett <davidpcorbett@gmail.com>
2017-04-15 00:44:57 +0000

    Baby Steps.

--- program.js
+++ program.js
@@ -1,1 +1,6 @@
-console.log('HELLO WORLD');
+console.log(process.argv); // DEBUG
+// Remove first two arguments.
+var operands = process.argv.slice(2);
+// Sum the remaining numbers.
+var result = operands.reduce((acc, val)=>parseInt(acc) + parseInt(val), 0);
+console.log(result); // DEBUG
