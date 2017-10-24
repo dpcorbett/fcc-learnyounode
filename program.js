109b57fab678bce97489bcc7a5e4c7e8982399d5
Dave Corbett <davidpcorbett@gmail.com>
2017-10-24 11:22:29 +0000

    Juggling Async

--- program.js
+++ program.js
@@ -1,27 +1,37 @@
-// Import required libraries.
-var bl = require('bl');
 var http = require('http');
-var addr = process.argv[2];
-// Retrieve data from URL supplied.
-http.get(addr, (res) => {
-  'use strict';
-  const  statusCode  = res.statusCode;
-  let error;
-  if (statusCode !== 200) {
-    error = new Error(`Request Failed.\n` +
-                      `Status Code: ${statusCode}`);
-  }
-  // Use Buffer List to read data.
-  res.pipe(bl(function (error, data) {
-    if (error) {
-      console.error(error.message);
-      // consume response data to free up memory
-      res.resume();
-      return;
-    }
-    console.log(data.length); // DEBUG
-    console.log(data.toString()); // DEBUG
-  }));
-}).on('error', (e) => {
-  console.error(`Got error: ${e.message}`);
-});
+var i,j;
+var urls = process.argv.slice(2);
+var results = [];
+
+// initialise results array
+for (i = 0; i < urls.length; i++) {
+  results[i] = null;
+}
+
+for (i = 0; i < urls.length; i++) {
+  
+  (function(i) {
+  
+    http.get(urls[i], function(request) {
+      var result = "";
+      request.setEncoding("utf8");
+      request.on("data", function(data) {
+        result += data;
+      });
+      request.on("end", function() {
+        results[i] = result;
+        var resultCount = 0;
+        for (j = 0; j < results.length; j++) {
+          if (results[j] != null) resultCount++;
+        }
+        if (resultCount == results.length) {
+          for (j = 0; j < results.length; j++) {
+            console.log(results[j]);
+          }
+        }
+      });
+    });
+	
+  })(i);
+
+}
