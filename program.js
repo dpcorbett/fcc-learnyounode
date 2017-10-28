5bef24a575f41b3ce9684b79cec08f7412eeda99
Dave Corbett <davidpcorbett@gmail.com>
2017-10-28 00:32:01 +0000

    HTTP JSON API Server

--- program.js
+++ program.js
@@ -1,9 +1,21 @@
-var http = require('http');  
-var map = require('through2-map')
+const http = require('http');  
+const url = require('url');
+
 var args = process.argv.slice(2);
 
 var server = http.createServer(function (req, res) { 
-  req.pipe(map((chunk) => {return chunk.toString().toUpperCase()})).pipe(res);
+  var route = url.parse(req.url, true);
+  var tempDate = new Date(route.query.iso);
+  console.log(route.pathname);
+  if (route.pathname === "/api/parsetime") {
+    tempDate = {hour: tempDate.getHours(), minute: tempDate.getMinutes(), second: tempDate.getSeconds()
+    };
+  }
+  if (route.pathname === "/api/unixtime") {
+    tempDate = {unixtime: tempDate.getTime()};
+  }
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(tempDate));
 });  
 
 
